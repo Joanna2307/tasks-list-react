0177167a27a3a86319a474e8999c1065a595a04d
Joanna Gorniak <joanna.gorniak.00@gmail.com>
2025-01-18 21:31:06 +0100

    content updated

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -4,7 +4,18 @@ import "./style.css";
 const Tasks = (props) => (
   <ul className="section__list">
     {props.tasks.map((task) => (
-      <li key={task.id}>{task.content}</li>
+      <li
+        key={task.id}
+        className={`${
+          props.hideDoneTasks && task.done ? "section__listItem--hidden" : ""
+        }`}
+      >
+        <button className="section__button">{task.done ? "✔" : ""}</button>
+        <span className={task.done ? "section__listItem--done" : ""}>
+          {task.content}
+        </span>
+        <button className="section__button section__buttonRemove">🗑</button>
+      </li>
     ))}
   </ul>
 );
