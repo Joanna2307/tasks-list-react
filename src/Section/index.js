372829400860a60ec54d939ca582394b0749f6b0
Joanna Gorniak <joanna.gorniak.00@gmail.com>
2025-01-22 12:56:43 +0100

    Buttons added

--- src/Section/index.js
+++ src/Section/index.js
@@ -5,12 +5,8 @@ const Section = ({ title, body, extraHeaderContent }) => (
   <section className="section">
     <div className="section__header--second">
       <h2 className="section__header">{title}</h2>
-
       {extraHeaderContent}
     </div>
-
-    <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
-
     <div className="section__tasksContainer">{body}</div>
   </section>
 );
